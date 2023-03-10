2cd590d1fd706de493860bd42e2f7a73b9063e88
Beksultan <96925396+Vazno@users.noreply.github.com>
2023-03-10 12:29:09 +0600

    Added compile.py venv for each compilation

---
+++ compile.py
@@ -0,0 +1,54 @@
+import os
+import shutil
+import sys
+
+def compile_windows():
+    # Get the absolute path to the icon file
+    icon_path = os.path.abspath("logo.ico")
+
+    # Create a new virtual environment
+    env_dir = os.path.abspath("venv_windows")
+    if os.path.exists(env_dir):
+        shutil.rmtree(env_dir)
+    os.system(f"{sys.executable} -m venv {env_dir}")
+
+    # Activate the virtual environment and install dependencies
+    activate_script = os.path.join(env_dir, "Scripts", "activate")
+    os.system(f"cmd /c {activate_script} && pip install -r requirements-windows.txt -r requirements.txt")
+
+    # Compile the code and create a single executable
+    output_path = os.path.abspath("dist/windows/myapp.exe")
+    command = f"python -m nuitka --standalone --output-file={output_path} --follow-imports --windows-icon-from-ico={icon_path} launch.py"
+    os.system(command)
+
+def compile_linux():
+    # Create a new virtual environment
+    env_dir = os.path.abspath("venv_linux")
+    if os.path.exists(env_dir):
+        shutil.rmtree(env_dir)
+    os.system(f"{sys.executable} -m venv {env_dir}")
+
+    # Activate the virtual environment and install dependencies
+    activate_script = os.path.join(env_dir, "bin", "activate")
+    os.system(f"source {activate_script} && pip install -r requirements.txt")
+
+    # Compile the code and create a single executable
+    output_path = os.path.abspath("dist/linux/myapp")
+    command = f"python -m nuitka --standalone --output-file={output_path} --follow-imports launch.py"
+    os.system(command)
+
+def compile_mac_os():
+    # Create a new virtual environment
+    env_dir = os.path.abspath("venv_macos")
+    if os.path.exists(env_dir):
+        shutil.rmtree(env_dir)
+    os.system(f"{sys.executable} -m venv {env_dir}")
+
+    # Activate the virtual environment and install dependencies
+    activate_script = os.path.join(env_dir, "bin", "activate")
+    os.system(f"source {activate_script} && pip install -r requirements.txt")
+
+    # Compile the code and create a single executable
+    output_path = os.path.abspath("dist/macos/myapp")
+    command = f"python -m nuitka --standalone --output-file={output_path} --plugin-enable=qt-plugins --plugin-enable=sqlite-extensions --follow-imports launch.py"
+    os.system(command)
--- compile.sh
+++
@@ -1,8 +0,0 @@
-# compile for macOS
-python -m nuitka --standalone --plugin-enable=qt-plugins --plugin-enable=sqlite-extensions --output-dir=dist/macos --follow-imports launch.py
-
-# compile for Linux
-python -m nuitka --standalone --output-dir=dist/linux --follow-imports launch.py
-
-# compile for Windows
-python -m nuitka --standalone --output-dir=dist/windows --follow-imports --windows-icon-from-ico logo.ico launch.py
